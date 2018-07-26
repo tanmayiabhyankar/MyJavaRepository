d5aadb34cb04a38cac0b5f4fac820a806df018a8
tanmayi abhyankar <tanmayi_abhyankar@persistent.co.in>
2018-07-26 12:40:02 +0530

    added method for displaying date

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,5 +1,10 @@
 class HelloWorld{
 
+void displayDate(){
+
+sysout("Display todays date...");
+}
+
 	public static void main(String[] args){
 
 	system.out.println("Hello World...Welcome to Git ");
