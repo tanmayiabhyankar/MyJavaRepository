946a23db7d52df61179d60161230bd7b8d31a2f3
tanmayi abhyankar <tanmayi_abhyankar@persistent.co.in>
2018-07-26 12:53:51 +0530

    Revert "added method for displaying date"
    This reverts commit d5aadb34cb04a38cac0b5f4fac820a806df018a8.

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,10 +1,5 @@
 class HelloWorld{
 
-void displayDate(){
-
-sysout("Display todays date...");
-}
-
 	public static void main(String[] args){
 
 	system.out.println("Hello World...Welcome to Git ");
