18fbe2a527da5bf30837be391931615856552b81
tanmayi abhyankar <tanmayi_abhyankar@persistent.co.in>
2018-07-26 15:08:56 +0530

    calling static displaydate

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,9 +1,14 @@
 class HelloWorld{
 
+	static void displayDate(){
+
+		sysout("Display todays date...");
+}
+
 	public static void main(String[] args){
 
 	system.out.println("Hello World...Welcome to Git ");
-
+	displayDate();
 
 	}
 }
